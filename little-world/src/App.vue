a7cdc86d89574a732091f79b991aa6b9c1f56bd8
Min-987 <n200127@gmail.com>
2024-12-28 00:48:51 +0800

    change to vue3 structure

---
+++ little-world/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>Hello! I'm Min.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+.hello {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+  font-family: Arial, sans-serif;
+  font-size: 2rem;
+}
+</style>
